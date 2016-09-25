9de80e8bb20fa5e6e8f30bb0482610cde1124754
Corentin Dancette <corentin.dancette@datadoghq.com>
2016-09-24 23:58:39 -0400

    base project

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROMM node:4-onbuild
+# replace this with your application's default port
+EXPOSE 80
+EXPOSE 443
---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+// returns an instance of node-letsencrypt with additional helper methods
+var lex = require('letsencrypt-express').create({
+  // set to https://acme-v01.api.letsencrypt.org/directory in production
+  server: 'staging'
+
+// If you wish to replace the default plugins, you may do so here
+//
+, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
+, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
+
+// You probably wouldn't need to replace the default sni handler
+// See https://github.com/Daplie/le-sni-auto if you think you do
+//, sni: require('le-sni-auto').create({})
+
+, approveDomains: approveDomains
+});
+
+function approveDomains(opts, certs, cb) {
+  // This is where you check your database and associated
+  // email addresses with domains and agreements and such
+
+
+  // The domains being approved for the first time are listed in opts.domains
+  // Certs being renewed are listed in certs.altnames
+  if (certs) {
+    opts.domains = certs.altnames;
+  }
+  else {
+    opts.email = 'john.doe@example.com';
+    opts.agreeTos = true;
+  }
+
+  cb(null, { options: opts, certs: certs });
+}
+
+// handles acme-challenge and redirects to https
+require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+  console.log("Listening for ACME http-01 challenges on", this.address());
+});
+
+
+
+var app = require('express')();
+app.use('/', function (req, res) {
+  res.end('Hello, World!');
+});
+
+// handles your app
+require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
+  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+});
+
+
+lex.register({
+  domains: ['test.coco.l4th.fr']                                // CHANGE TO YOUR DOMAIN (list for SANS)
+  , email: 'admin@l4th.fr'                                 // CHANGE TO YOUR EMAIL
+  , agreeTos: true                                            // set to tosUrl string (or true) to pre-approve (and skip agreeToTerms)
+  , rsaKeySize: 2048                                        // 2048 or higher
+  , challengeType: 'http-01'                                // http-01, tls-sni-01, or dns-01
+
+  }).then(function (results) {
+    console.log('success');
+  }, function (err) {
+
+    // Note: you must either use le.middleware() with express,
+    // manually use le.challenges['http-01'].get(opts, domain, key, val, done)
+    // or have a webserver running and responding
+    // to /.well-known/acme-challenge at `webrootPath`
+    console.error('[Error]: node-letsencrypt/examples/standalone');
+    console.error(err.stack);
+
+  });
+});
